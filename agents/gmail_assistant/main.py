712150765228df316a59e36fd00e86ba6f2799b2
Dan Morris <damorri@microsoft.com>
2023-03-11 13:41:13 -0800

    remove unneeded functions

--- agents/gmail_assistant/main.py
+++ agents/gmail_assistant/main.py
@@ -34,7 +34,7 @@ and generate responses to those emails. \
 events by their title and time, unless the user asks for attendees or location."""
 
 
-def getFewShots():
+"""def getFewShots():
     import os
     folder_path = "C:/fixie/fixie-hack23/agents/trainingdata" # replace with the path to your folder containing text files
     files = os.listdir(folder_path)
@@ -62,7 +62,7 @@ def getFewShots():
         fewShots.add("Q: Answer the email " + value +  "[FUNCS]" + "A: " +
             SentEmailsdata)
         
-    return fewShots                          
+    return fewShots     """                     
 
 FEW_SHOTS = """
 Q: Answer this email from today? "hi dan how is your day"
@@ -108,104 +108,3 @@ def answeremail(query: fixieai.Message):
 def workhours():
     """Returns valid working hours. Currently simple."""
     return "09:00 AM to 05:00 PM"
-
-
-@agent.register_func
-def events(query: fixieai.Message, oauth_handler: fixieai.OAuthHandler) -> str:
-    """Returns all events in user's calendar for a range.
-
-    query should be in the format:
-        {
-            "start": "Weekday Month DD YYYY HH:MM",  # default = now
-            "end": "Weekday Month DD YYYY HH:MM",    # default = eternity
-        }
-    """
-    user_token = oauth_handler.user_token()
-    if user_token is None:
-        return oauth_handler.get_authorization_url()
-
-    client = gcalendar_client.GcalendarClient(user_token)
-    range_json = query.text
-    range_dict = json.loads(range_json)
-    start = utils.parse_datetime(range_dict.get("start")) or datetime.datetime.utcnow()
-    end = utils.parse_datetime(range_dict.get("end"))
-    events = client.events(start=start, end=end)
-    if events:
-        return "\n".join(f"{event.show_in(utils.USER_TIMEZONE)}" for event in events)
-    else:
-        return "You don't have anything scheduled."
-
-
-@agent.register_func
-def availability(query: fixieai.Message, oauth_handler: fixieai.OAuthHandler) -> str:
-    """Returns a list of possible start datetimes for an event of custom length.
-
-    query should be in the format:
-        {
-            "start": "Weekday Month DD YYYY HH:MM",  # default = now
-            "end": "Weekday Month DD YYYY HH:MM",    # default = now + 1 day
-            "duration": "HH:MM:SS"                   # default = 30 min
-        }
-    """
-    user_token = oauth_handler.user_token()
-    if user_token is None:
-        return oauth_handler.get_authorization_url()
-
-    client = gcalendar_client.GcalendarClient(user_token)
-    query_json = json.loads(query.message.just_str())
-    start = utils.parse_datetime(query_json.get("start")) or datetime.datetime.utcnow()
-    end = utils.parse_datetime(query_json.get("end")) or (
-        start + datetime.timedelta(days=1)
-    )
-    duration = utils.parse_timedelta(query_json.get("duration"))
-
-    # List all events in the asked [start, end] range and find available slots.
-    booked_events = client.events(start=start, end=end)
-    possible_starts = utils.find_available_slots(booked_events, start, end, duration)
-
-    # Report back the options.
-    if possible_starts:
-        return str(
-            [
-                gcalendar_client.to_localtime_str(possible_start, utils.USER_TIMEZONE)
-                for possible_start in possible_starts
-            ]
-        )
-    else:
-        return "There's no availability on the selected dates for the given duration."
-
-
-@agent.register_func
-def schedule(query: fixieai.Message, oauth_handler: fixieai.OAuthHandler) -> str:
-    """Schedules a new meeting at a given time with custom title.
-
-    query should be in the format:
-        {
-            "start": "Weekday Month DD YYYY HH:MM",  # default = now
-            "duration": "HH:MM:SS",                  # default = 30 min
-            "title": "Title",                        # default = Untitled
-            "attendees": ["john@smith.com", ...],    # default = []
-            "description": "Description"             # default = ""
-        }
-    """
-    user_token = oauth_handler.user_token()
-    if user_token is None:
-        return oauth_handler.get_authorization_url()
-
-    client = gcalendar_client.GcalendarClient(user_token)
-    range_json = query.message.just_str()
-    range_dict = json.loads(range_json)
-    start = utils.parse_datetime(range_dict.get("start")) or datetime.datetime.utcnow()
-    duration = utils.parse_timedelta(range_dict.get("duration"))
-
-    title = range_dict.get("title") or "Untitled"
-    attendees = range_dict.get("attendees") or []
-    description = range_dict.get("description") or ""
-    event = client.create_event(
-        start=start,
-        duration=duration,
-        title=title,
-        attendees=attendees,
-        description=description,
-    )
-    return f"{event.show_in(utils.USER_TIMEZONE)} is scheduled."
